80be4ee387cd16a602dec3b92d8d76c5d46138de
Vonaista <86628538+Vonaista@users.noreply.github.com>
2022-04-12 21:55:48 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -1,4 +1,4 @@
-# Telegramda yani ben boş işler müdürü :) <> @Mahoaga Tarafından düzenlenen ufak çaplı proje. 
+# Telegramda <> @EfsaneLions Tarafından düzenlenen ufak çaplı proje. 
 import os
 import requests
 import aiohttp
